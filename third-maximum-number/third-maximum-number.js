01881855eab4ce7c322d03dbe22d0deba8da3936
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-03-01 10:21:55 -0600

    Time: 184 ms (5.07%), Space: 47.5 MB (5.07%) - LeetHub

---
+++ third-maximum-number/third-maximum-number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ 
+ 
+ I: int array
+ 0: third max number, if array.length < 3 -> Max number
+ C: 1 to 10000
+ E:
+ */
+
+var thirdMax = function(nums) {
+  let max = nums[0];
+  let sec = Number.MIN_SAFE_INTEGER;
+  let third = Number.MIN_SAFE_INTEGER;
+  
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    if(num > max) {
+      third = sec;
+      sec = max;
+      max = num;
+    } 
+    if (num > sec && num < max ) {
+      third = sec;
+      sec = num;
+    } 
+    if (num > third && num < sec) {
+      third = num;
+    }
+    console.log(max, sec, third)
+  }
+  
+  return third === Number.MIN_SAFE_INTEGER ? max : third;
+};
